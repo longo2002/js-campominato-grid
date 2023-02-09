bf433a4116993cbc408ab2c0d7c8012ea403d369
longo2002 <lombardo.fra2002@gmail.com>
2023-02-09 16:08:50 +0100

    done + bonus

--- js/app.js
+++ js/app.js
@@ -3,34 +3,30 @@ let btnEl = document.getElementById("bottone")
 
 bottone.addEventListener("click",
     function myFunction() {
-        let latoGriglia = 8
-        let numeroCelle = latoGriglia * latoGriglia
+        btnEl.classList.add("nodisplay")
 
-        console.log(latoGriglia, numeroCelle)
+        
+        let gridSide = 10
+        let playArea = gridSide * gridSide
+        const gridEl = document.querySelector('.griglia')
 
-        const grigliaElement = document.querySelector('.griglia')
-        console.log(grigliaElement)
-
-        for (let i = 0; i < numeroCelle; i++) {
+        for (let i = 0; i < playArea; i++) {
             let num = i + 1
             console.log(num)
-            let divString = `<div class="cella" style="width: calc(100% / ${latoGriglia});" >${num}</div>`
-            grigliaElement.innerHTML += divString
+            let divString = `<div class="cella" style="width: calc(100% / ${gridSide});" >${num}</div>`
+            gridEl.innerHTML += divString
 
         }
 
-        const celleElements = document.querySelectorAll('.cella')
+        const cellEl = document.querySelectorAll('.cella')
 
-        for (let i = 0; i < celleElements.length; i++) {
-            const cella = celleElements[i]
+        for (let i = 0; i < cellEl.length; i++) {
+            const cell = cellEl[i]
 
-            cella.addEventListener('click', function () {
+            cell.addEventListener('click', function () {
+                cell.classList.add("indicatore")
                 console.log(i + 1)
             })
-        }
-
-        function onClick() {
-            console.log(i + 1)
         }
     }
 )
